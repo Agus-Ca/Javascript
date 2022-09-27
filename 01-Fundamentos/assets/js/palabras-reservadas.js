9c7f010d982c5b3a487e39094b04ef01d4e034dd
Agustín Casado <agu.e.casado@gmail.com>
2022-09-26 21:19:41 -0300

    palabras reservadas

---
+++ 01-Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,16 @@
+// Pagina para ver si el nombre de una variable es valido
+let link1 = 'https://mothereff.in/js-variables#%E0%B2%A0%5f%E0%B2%A0'
+
+// Pagina de nombres raros para variables
+let link2 = 'https://mathiasbynens.be/notes/javascript-identifiers'
+
+// let const = 123;
+let constante = 123;
+
+// normas
+// empezar las variables con letras o guion bajo
+// se pueden poner numeros pero no al principio
+// no poner caracteres especiales, salvo signo $
+// no usar ñ
+// lower camelcase para variables
+// upper camelcase para clases
