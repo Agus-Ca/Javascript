c0196152e41115c00947095f00f9df8bc9bcdde5
Agustín Casado <agu.e.casado@gmail.com>
2022-10-04 13:36:24 -0300

    metodos en las clases

--- 04-Clases y ESNext private properties/assets/js/classes/class.js
+++ 04-Clases y ESNext private properties/assets/js/classes/class.js
@@ -11,11 +11,26 @@ class Persona {
     nombre = ``;
     codigo = ``;
     frase = ``;
+
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase() {
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
 }
 
 let spiderman = new Persona();
 console.log( spiderman );
 spiderman = new Persona( `Peter Parker`, `Spiderman`, `El amigable hombre araña` );
 console.log( spiderman );
+
 const ironman = new Persona(`Tony Stark`, `IronMan`, `Yo soy IronMan`);
 console.log( ironman );
+
+spiderman.quienSoy();
+ironman.quienSoy();
+
+spiderman.miFrase();
+ironman.miFrase();
