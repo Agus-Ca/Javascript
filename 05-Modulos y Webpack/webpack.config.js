2ede1ed853c318e81cbe4594d0072ffaed63b669
Agustín Casado <agu.e.casado@gmail.com>
2022-10-06 11:00:29 -0300

    plugin: html loader

--- 05-Modulos y Webpack/webpack.config.js
+++ 05-Modulos y Webpack/webpack.config.js
@@ -1,11 +1,32 @@
+const HtmlWebpack = require('html-webpack-plugin');
+
+
 module.exports = {
     mode: 'development',
 
+    output: {
+        clean: true // limpia dist
+    },
+
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
     },
 
     optimization: {},
 
-    plugins: [],
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App', // title de la app
+            filename: 'index.html', // nombre del file de html
+            template: './src/index.html'// archivo de cual se basa
+        })
+    ],
 }
