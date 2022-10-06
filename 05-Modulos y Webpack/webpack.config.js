b119e470e4fd01e548c58e72bf6224a90769a3f2
Agustín Casado <agu.e.casado@gmail.com>
2022-10-06 13:23:55 -0300

    archivo de estilos global

--- 05-Modulos y Webpack/webpack.config.js
+++ 05-Modulos y Webpack/webpack.config.js
@@ -1,4 +1,6 @@
-const HtmlWebpack = require('html-webpack-plugin');
+const HtmlWebpack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+
 
 
 module.exports = {
@@ -19,7 +21,12 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                exclude: /styles\.css$/,
                 use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ]
             }
         ]
     },
@@ -31,6 +38,11 @@ module.exports = {
             title: 'Mi Webpack App', // title de la app
             filename: 'index.html', // nombre del file de html
             template: './src/index.html'// archivo de cual se basa
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
         })
     ],
 }
