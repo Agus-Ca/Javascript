00811d8910fcf48bc7a7b7bae02297ccbd0ebbe9
Agustín Casado <agu.e.casado@gmail.com>
2022-10-12 23:23:14 -0300

    promise.race

--- 07-Callbacks y Promesas/src/index.js
+++ 07-Callbacks y Promesas/src/index.js
@@ -1,44 +1,12 @@
-import './styles.css';
-import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
-import { buscarHeroe } from './js/promesas';
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
 
 
+console.warn('---- Promise.race ----');
 
-console.warn('---- Callbacks ----');
+promesaLenta.then( console.log );
+promesaMedia.then( console.log );
+promesaRapida.then( console.log );
 
-const heroeId = 'capi';
-const heroeId2 = 'iron';
-
-buscarHeroeCallback( heroeId, ( err, heroe ) => {
-    if( err ) { return console.warn( err ); }
-
-    buscarHeroeCallback( heroeId2, ( err2, heroe2 ) => {
-        if( err2 ) { return console.warn( err2 ); }
-
-        console.info(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision.`);
-    });
-});
-
-
-console.warn('---- Promesas ----');
-
-// buscarHeroe( heroeId ).then( heroe => {
-
-//     buscarHeroe( heroeId2 ).then( heroe2 => {
-//         console.info(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision.`);
-//     });
-// });
-
-Promise.all([ buscarHeroe(heroeId), buscarHeroe(heroeId2) ])
-    .then( ([heroe1, heroe2]) => {
-        // esto se ejecuta si ninguna promesa falla
-        console.info(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision.`);
-}).catch( err => {
-    alert(err);
-}).finally( () => {
-    console.log('Se termino el promise.all');
-});
-
-
-
-console.warn('Fin del programa...');
+Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
+       .then( console.log )
+       .catch( console.warn );
---
+++ 07-Callbacks y Promesas/src/js/uso-callbacks-promesas.js
@@ -0,0 +1,44 @@
+import './styles.css';
+import { buscarHeroe as buscarHeroeCallback } from './callbacks';
+import { buscarHeroe } from './promesas';
+
+
+
+console.warn('---- Callbacks ----');
+
+const heroeId = 'capi';
+const heroeId2 = 'iron';
+
+buscarHeroeCallback( heroeId, ( err, heroe ) => {
+    if( err ) { return console.warn( err ); }
+
+    buscarHeroeCallback( heroeId2, ( err2, heroe2 ) => {
+        if( err2 ) { return console.warn( err2 ); }
+
+        console.info(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision.`);
+    });
+});
+
+
+console.warn('---- Promesas ----');
+
+// buscarHeroe( heroeId ).then( heroe => {
+
+//     buscarHeroe( heroeId2 ).then( heroe2 => {
+//         console.info(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision.`);
+//     });
+// });
+
+Promise.all([ buscarHeroe(heroeId), buscarHeroe(heroeId2) ])
+    .then( ([heroe1, heroe2]) => {
+        // esto se ejecuta si ninguna promesa falla
+        console.info(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision.`);
+}).catch( err => {
+    alert(err);
+}).finally( () => {
+    console.log('Se termino el promise.all');
+});
+
+
+
+console.warn('Fin del programa...');
