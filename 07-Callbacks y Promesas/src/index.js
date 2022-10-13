11aaeb1274a6ae78fd5151eef6af4202a4fe5d79
Agustín Casado <agu.e.casado@gmail.com>
2022-10-12 22:17:15 -0300

    callback error

--- 07-Callbacks y Promesas/src/index.js
+++ 07-Callbacks y Promesas/src/index.js
@@ -6,7 +6,14 @@ import { buscarHeroe } from './js/callbacks'
 console.warn('---- Callbacks y Promesas ----')
 
 const heroeId = 'capi';
+const heroeId2 = 'iron';
 
-buscarHeroe( heroeId, ( heroe ) => {
-    console.log( heroe );
+buscarHeroe( heroeId, ( err, heroe ) => {
+    if( err ) {
+        console.error( err );
+    } else {
+        console.info( heroe );
+    }
 });
+
+console.warn('Fin del programa...');
