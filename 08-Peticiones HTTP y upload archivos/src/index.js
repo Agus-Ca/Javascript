93a20d47600f6c8bc6f1c70b9ab7636c1157c0c4
Agustín Casado <agu.e.casado@gmail.com>
2022-10-15 14:52:40 -0300

    refactor: promesas en cadena

--- 08-Peticiones HTTP y upload archivos/src/index.js
+++ 08-Peticiones HTTP y upload archivos/src/index.js
@@ -4,9 +4,15 @@ import './styles.css';
 
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch( jokeUrl ).then( resp => {
-    resp.json().then( ({ id, value }) => {
-        console.log( id );
-        console.log( value );
+// fetch( jokeUrl ).then( resp => {
+//     resp.json().then( ({ id, value }) => {
+//         console.log( id );
+//         console.log( value );
+//     });
+// });
+
+fetch( jokeUrl )
+    .then( resp => resp.json() )
+    .then( ({ id, value }) => {
+        console.log({ id, value });
     });
-});
