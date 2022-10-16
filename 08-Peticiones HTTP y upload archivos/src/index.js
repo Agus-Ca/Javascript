a55cd70d4d04e5bdad6f878164e9f5c268f76d03
Agustín Casado <agu.e.casado@gmail.com>
2022-10-15 23:06:09 -0300

    crud - actualizar usuario

--- 08-Peticiones HTTP y upload archivos/src/index.js
+++ 08-Peticiones HTTP y upload archivos/src/index.js
@@ -1,5 +1,8 @@
 import * as CRUD from './js/crud-provider';
 
 
+// CRUD.getUsuario( 2 ).then( console.log );
 
-CRUD.getUsuario( 1 ).then( console.log );
+// CRUD.crearUsuario({ name: 'Agustin', job: 'Sw Dev'}).then( console.log );
+
+CRUD.actualizarUsuario( 1, { name: 'Melisa', job: 'Developer'}).then( console.log );
